438bfb04c6e04e7ae6672bb87c89b75706800928
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-03-10 23:21:21 +0300

    Added Time text and start-stop buttons

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,6 +10,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          child: Center(
+            child: Text(
+              '1:24:20',
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Icon(Icons.play_arrow)),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(onPressed: () {}, child: Icon(Icons.stop)),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
